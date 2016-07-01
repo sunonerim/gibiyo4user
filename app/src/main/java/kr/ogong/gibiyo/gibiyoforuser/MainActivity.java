ba9c65ad4efcbde1794988a4429c9aa033ae1637
Sungwon Rim <sunonerim@gmail.com>
2016-07-01 12:47:21 +0900

    back key 처리

--- app/src/main/java/kr/ogong/gibiyo/gibiyoforuser/MainActivity.java
+++ app/src/main/java/kr/ogong/gibiyo/gibiyoforuser/MainActivity.java
@@ -1,9 +1,5 @@
 package kr.ogong.gibiyo.gibiyoforuser;
 
-import android.app.Activity;
-import android.content.ActivityNotFoundException;
-import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,20 +9,11 @@ import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.onesignal.OneSignal;
 
 
 public class MainActivity extends AppCompatActivity {
     private WebView webview;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,49 +42,6 @@ public class MainActivity extends AppCompatActivity {
 
         // OneSignal start.... hehe
         OneSignal.startInit(this).init();
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://kr.ogong.gibiyo.gibiyoforuser/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://kr.ogong.gibiyo.gibiyoforuser/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
     }
 
     @Override
@@ -109,40 +53,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     class WebClient extends WebViewClient {
-        public static final String INTENT_PROTOCOL_START = "intent:";
-        public static final String INTENT_PROTOCOL_INTENT = "#Intent;";
-        public static final String INTENT_PROTOCOL_END = ";end;";
-        public static final String GOOGLE_PLAY_STORE_PREFIX = "market://details?id=";
-
-        @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if (url.startsWith(INTENT_PROTOCOL_START)) {
-                final int customUrlStartIndex = INTENT_PROTOCOL_START.length();
-                final int customUrlEndIndex = url.indexOf(INTENT_PROTOCOL_INTENT);
-                if (customUrlEndIndex < 0) {
-                    return false;
-                } else {
-                    final String customUrl = url.substring(customUrlStartIndex, customUrlEndIndex);
-                    try {
-
-                        ((Activity) view.getContext()).startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(customUrl)));
-                    } catch (ActivityNotFoundException e) {
-                        final int packageStartIndex = customUrlEndIndex + INTENT_PROTOCOL_INTENT.length();
-                        final int packageEndIndex = url.indexOf(INTENT_PROTOCOL_END);
-
-                        final String packageName = url.substring(packageStartIndex, packageEndIndex < 0 ? url.length() : packageEndIndex);
-                        view.getContext().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(GOOGLE_PLAY_STORE_PREFIX + packageName)));
-                    }
-                    return true;
-                }
-            } else if (url.startsWith("tel:")) {
-                //tel:01000000000
-                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
-                startActivity(intent);
-                return true;
-            } else {
-                return false;
-            }
+            view.loadUrl(url);
+            return true;
         }
     }
 }
